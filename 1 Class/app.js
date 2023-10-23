0d77ac6ba9aa8f68e19b7aa39c1346ece4cea360
hamza043 <muhamadhamza474@gmail.com>
2023-10-23 16:44:19 +0500

    first javascript assigment

---
+++ 1 Class/app.js
@@ -0,0 +1,38 @@
+console.log("Hellow World");
+
+// alert('this is alert box');
+
+// var names = "Muhammad Hamza"
+
+// var num = 2;
+
+// var num2 = 2 + 4;
+// let message = alert('you are in javascript file');
+
+// let name = "Muhammad Hamza";
+// alert("Hello, " + name + "!");
+
+// console.log(num2);
+// let userName = prompt('what is your name');
+// console.log(userName);
+
+// let num = 2;
+// num += 4;
+// console.log(num);
+
+// let num = 2;
+// num += 4;
+// num -= 1;
+// console.log(num);
+
+var studentName = prompt("Enter Student Name:");
+var marksObtained = Number(prompt("Enter Marks Obtained:"));
+var totalMarks = Number(prompt("Enter Total Marks:"));
+var percentage = (marksObtained / totalMarks) * 100;
+var grade = (percentage >= 90 && 'A') || (percentage >= 80 && 'B') || (percentage >= 70 && 'C') || (percentage >= 60 && 'D') || 'F';
+
+console.log("Student Name: " + studentName);
+console.log("Marks Obtained: " + marksObtained);
+console.log("Total Marks: " + totalMarks);
+console.log("Percentage: " + percentage.toFixed(2) + "%");
+console.log("Grade: " + grade);
